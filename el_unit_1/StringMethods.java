0d332a3b9ae016213192f9c5d62ad21cd2f19aeb
lalithbseervi <lcoding20@gmail.com>
2026-09-09 22:09:05 +0530

    updated unit 1 el programs

--- el_unit_1/StringMethods.java
+++ el_unit_1/StringMethods.java
@@ -2,35 +2,41 @@ package el_unit_1;
 
 public class StringMethods {
     public static void main(String[] args) {
-        String str = "  Java Programming  ";
-        String str2 = "JAVA";
+        String str = "  Casey Muratori  ";
+        String str2 = "Java Virtual Machine";
 
-        System.out.println("Original: " + str);
         System.out.println("Length: " + str.length());
 
         String trimmed = str.trim();
         System.out.println("Trimmed: " + trimmed);
+
         System.out.println("Uppercase: " + trimmed.toUpperCase());
         System.out.println("Lowercase: " + trimmed.toLowerCase());
-        System.out.println("Character at 2: " + trimmed.charAt(2));
+
+        System.out.println("Character at index 2 of trimmed str: " + trimmed.charAt(2));
+
         System.out.println("Equals: " + trimmed.equals(str2));
         System.out.println("Equals Ignore Case: " + trimmed.equalsIgnoreCase(str2));
-        System.out.println("Contains 'Prog': " + trimmed.contains("Prog"));
+        System.out.println("Contains 'Mura': " + trimmed.contains("Mura"));
+
         System.out.println("Index of 'a': " + trimmed.indexOf('a'));
         System.out.println("Last Index of 'a': " + trimmed.lastIndexOf('a'));
+
         System.out.println("Substring(5): " + trimmed.substring(5));
         System.out.println("Substring(0,4): " + trimmed.substring(0,4));
+
         System.out.println("Replace a with o: " + trimmed.replace('a','o'));
-        System.out.println("Starts with Java: " + trimmed.startsWith("Java"));
-        System.out.println("Ends with ming: " + trimmed.endsWith("ming"));
+
+        System.out.println("Starts with Case: " + trimmed.startsWith("Case"));
+        System.out.println("Ends with ing: " + trimmed.endsWith("ing"));
 
         String[] words = trimmed.split(" ");
-        System.out.println("Words:");
+        System.out.println("Words upon splitting by space:");
         for(String w : words)
             System.out.println(w);
         
         char[] chars = trimmed.toCharArray();
-        System.out.println("Characters:");
+        System.out.println("Printing individual characters:");
         for(char c : chars)
             System.out.print(c + " ");    
         System.out.println();    
--- el_unit_1/sum_of_products.java
+++ el_unit_1/sum_of_products.java
@@ -1,9 +1,20 @@
 package el_unit_1;
 
+import java.util.Scanner;
+
 public class sum_of_products {
     public static void main(String[] args) {
-        int prices[] = { 30, 160, 95, 56 };
+        int array_size = 5;
+        int prices[] = new int[array_size];
         int sum = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < array_size; i++) {
+            System.out.print("Enter price for product " + (i + 1) + ": ");
+            prices[i] = sc.nextInt();
+        }
+
         for (int n : prices)
             sum += n;
         System.out.println("Final bill amount: " + sum);
